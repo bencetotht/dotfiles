95a97ba5beb6caec363f9cd7cdbb26d7dfe06f2d
Bence Toth <tothbencetamas@gmail.com>
2025-10-20 23:25:57 +0200

    ghostty conf

--- nix/nixos/users/bence/home-manager.nix
+++ nix/nixos/users/bence/home-manager.nix
@@ -38,22 +38,23 @@ in {
     shellAliases = shellAliases;
   };
 
-  programs.alacritty = {
-    enable = true;
-
-    settings = {
-      env.TERM = "xterm-256color";
-
-      key_bindings = [
-        { key = "K"; mods = "Command"; chars = "ClearHistory"; }
-        { key = "V"; mods = "Command"; action = "Paste"; }
-        { key = "C"; mods = "Command"; action = "Copy"; }
-        { key = "Key0"; mods = "Command"; action = "ResetFontSize"; }
-        { key = "Equals"; mods = "Command"; action = "IncreaseFontSize"; }
-        { key = "Subtract"; mods = "Command"; action = "DecreaseFontSize"; }
-      ];
-    };
-  };
+#   programs.alacritty = {
+#     enable = true;
+#
+#     settings = {
+#       env.TERM = "xterm-256color";
+#
+#       key_bindings = [
+#         { key = "K"; mods = "Command"; chars = "ClearHistory"; }
+#         { key = "V"; mods = "Command"; action = "Paste"; }
+#         { key = "C"; mods = "Command"; action = "Copy"; }
+#         { key = "Key0"; mods = "Command"; action = "ResetFontSize"; }
+#         { key = "Equals"; mods = "Command"; action = "IncreaseFontSize"; }
+#         { key = "Subtract"; mods = "Command"; action = "DecreaseFontSize"; }
+#       ];
+#     };
+#   };
+  programs.ghostty.enable = true;
 
   home.pointerCursor = lib.mkIf (isLinux) {
     name = "Vanilla-DMZ";
