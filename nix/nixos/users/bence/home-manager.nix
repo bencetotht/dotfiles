50fa4b7c47244de5b7df538d02b943b19d20d477
Bence Toth <36576531+bencetotht@users.noreply.github.com>
2025-10-20 23:18:02 +0200

    add homemanager configs

--- nix/nixos/users/bence/home-manager.nix
+++ nix/nixos/users/bence/home-manager.nix
@@ -2,13 +2,65 @@
 
 { config, lib, pkgs, ... }:
 
-{
+let
+  isDarwin = pkgs.stdenv.isDarwin;
+  isLinux = pkgs.stdenv.isLinux;
+
+  shellAliases = {
+    lg = "lazygit";
+  } // (if isLinux then {
+    pbcopy = "xclip";
+    pbpaste = "xclip -o";
+  } else {});
+
+in {
   # imports = [ ./neovim.nix ];
 
-  # home.username = "bence";
-  # home.homeDirectory = "/home/bence";
+  home.username = "bence";
+  home.homeDirectory = "/home/bence";
 
-  # programs.zsh.enable = true;
+  home.packages = [
+    pkgs.xfce.xfce4-terminal
+    pkgs.eza
+    pkgs.fd
+    pkgs.fzf
+  ];
+
+  # home.sessionVariables = {
+  #   LANG = "en_US.UTF-8";
+  #   LC_CTYPE = "en_US.UTF-8";
+  #   LC_ALL = "en_US.UTF-8";
+  #   EDITOR = "nvim";
+  # };
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = shellAliases;
+  };
+
+  programs.alacritty = {
+    enable = true;
+
+    settings = {
+      env.TERM = "xterm-256color";
+
+      key_bindings = [
+        { key = "K"; mods = "Command"; chars = "ClearHistory"; }
+        { key = "V"; mods = "Command"; action = "Paste"; }
+        { key = "C"; mods = "Command"; action = "Copy"; }
+        { key = "Key0"; mods = "Command"; action = "ResetFontSize"; }
+        { key = "Equals"; mods = "Command"; action = "IncreaseFontSize"; }
+        { key = "Subtract"; mods = "Command"; action = "DecreaseFontSize"; }
+      ];
+    };
+  };
+
+  home.pointerCursor = lib.mkIf (isLinux) {
+    name = "Vanilla-DMZ";
+    package = pkgs.vanilla-dmz;
+    size = 128;
+    x11.enable = true;
+  };
 
   home.stateVersion = "25.05";
 }
