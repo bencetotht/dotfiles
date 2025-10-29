1f22682d1744144913a666cec2b645783ed484de
Bence Toth <36576531+bencetotht@users.noreply.github.com>
2025-10-29 17:19:26 +0100

    - bantime

--- nix/nixnas/modules/nas/configuration.nix
+++ nix/nixnas/modules/nas/configuration.nix
@@ -100,13 +100,6 @@
     enable = true;
     maxretry = 5;
     bantime = "24h";
-    bantime-increment = {
-      enable = true;
-      formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
-      multipliers = "1 2 4 8 16 32 64";
-      maxtime = "168h";
-      overalljails = true;
-    };
   };
 
   # Enable tailscale
