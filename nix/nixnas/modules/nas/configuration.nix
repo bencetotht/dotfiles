1f4b8f73000c7b196caa98baaff083ff07a6f16f
Bence Toth <36576531+bencetotht@users.noreply.github.com>
2025-10-29 11:14:08 +0100

    fix

--- nix/nixnas/modules/nas/configuration.nix
+++ nix/nixnas/modules/nas/configuration.nix
@@ -71,7 +71,7 @@
     shell = pkgs.zsh;
     hashedPassword = "$6$pIsEjWjJ5M46WA1c$G8daVmgyAy2fJ62t/ceDkMWSqBVRMPVJS5j2a3q5dvEzOYplV6DYpLNxb7tjgAM9QRC7f2Y09YRbAmJCLMOMs1";
     home = "/home/bence";
-    sshAuthorizedKeys = [
+    openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCpo3vH1A9UZFJ+Z3HxuDcE7bQvfRpzgmNpbdbMPjTqEuXbtse1qYs8J1WEJ16iyac7236RJdgAHyx4LqVgdUKL1EhoHVqU1dux9dCLyLLIzPWiTdCLEYUKVCcj7/UUU3cmC3BlC1KQjTILcxH4L0+5P3877O6mY9ghHnBmbEHNR5BMTWLt6mYlrV0hSM2tb3sUcq6OsilRHjMTKnS/rBwhqLS9SQrhsV3Om+TMWiAsDRzLpqmA28PNH8oabPMzUkrc4YNnEVS14UlwvpGr96ezzBDNEIhYkHVOOJ3PftMw9mSs1Ar+KzXfSwy7u+k1BSsjTNqlJobrJoFVc+jIjV1svog5tfOPWU5t1VTpIdCqKw/xJd9UJZ8va1xDibVQ/L718X5MACNURCO23KxporqCKM2k13J47EJnR+bXAck28uISrEoTffwfj7IMaEf+Av311PMkqT+FSOzYvJZgd2NKX9dM/J+2J6NPvmsrwiHdPLmBWeFMGE6mpbwYBs/woBq7KztNUp925+vjqHEVI0Sn1xX471Om/ZxdoU3UT+rSkXnKFaH8r7YpqDfymYTnzGnQwT1Va6NYWN4mL8+CMJTXGg5urk88pb/fHRjqOA9ltXGsX6okqIfsZDyQo1LQewcZkqcoxR6ycMeFDekgAiDIGAIRBhED7dFpZ+ylcoHWXw=="
     ];
   };
