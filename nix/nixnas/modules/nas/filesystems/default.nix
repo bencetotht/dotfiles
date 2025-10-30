200ad606f333a1ddb696fc7ebe1a77eda13490a6
Bence Toth <36576531+bencetotht@users.noreply.github.com>
2025-10-30 22:33:19 +0100

    mergerfs

--- nix/nixnas/modules/nas/filesystems/default.nix
+++ nix/nixnas/modules/nas/filesystems/default.nix
@@ -16,6 +16,7 @@
   # fixing mergerfs permission issues
   # boot.initrd.systemd.enable = true;
 
+  # Disk definitions
   fileSystems."/mnt/w3d3" = {
     device = "/dev/disk/by-label/w3d3";
     fsType = "ext4";
@@ -23,5 +24,18 @@
   fileSystems."/mnt/w3d4" = {
     device = "/dev/disk/by-label/w3d4";
     fsType = "ext4";
+  };
+
+  # Mergerfs
+  fileSystems."/mnt/w3storage" = {
+    device = "/mnt/w3d3:/mnt/w3d4";
+    fsType = "fuse.mergerfs";
+    options = [
+      "defaults"
+      "allow_other"
+      "use_ino"
+      "category.create=mfs"
+      "moveonenospc=true"
+    ];
   };
 }
