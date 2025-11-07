dda809ecf726bbd21952098d6ea49cdeaa027df0
Bence Toth <36576531+bencetotht@users.noreply.github.com>
2025-11-07 10:57:31 +0100

    major changes

--- nix/nixnas/modules/nas/shares/samba.nix
+++ nix/nixnas/modules/nas/shares/samba.nix
@@ -9,23 +9,29 @@
     package = pkgs.samba4Full;
     openFirewall = true; # allow samba to be accessed from the internet
 
-    extraConfig = ''
-      server min protocol = SMB3
-      vfs objects = fruit streams_xattr
-      fruit:metadata = stream
-      fruit:resource = file
-      fruit:time machine = yes
-      fruit:locking = none
-      fruit:encoding = native
-    '';
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "NIXNAS";
+        "server min protocol" = "SMB3";
+        "security" = "user";
+        "map to guest" = "Bad User";
+        "vfs objects" = "catia fruit streams_xattr";
+        "fruit:aapl" = "yes";
+        "fruit:model" = "TimeMachineNAS";
+        "fruit:metadata" = "stream";
+        "fruit:time machine" = "yes";
+      };
+    };
 
     shares = {
       "timemachine" = {
         path = "/mnt/w3storage/tmbackup";
         browseable = "yes";
         writeable = "yes";
-        "valid users" = "tmuser";
+        "valid users" = [ "tmuser" ];
         "fruit:time machine" = "yes";
+        "fruit:time machine max size" = "500G";
       };
     };
   };
